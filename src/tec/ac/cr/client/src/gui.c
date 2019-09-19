b2beee3fcd2f9c8da49e85c43e44040d95e49d0f
Jose David <jose.david.san@gmail.com>
2019-09-18 23:55:03 -0600

    Se trabaja la interfaz.

--- src/tec/ac/cr/client/src/gui.c
+++ src/tec/ac/cr/client/src/gui.c
@@ -6,5 +6,8 @@
 
 void init_screen(){
     pantalla = al_create_display(ANCHO_PANTALLA, ALTO_PANTALLA);
-    fondo = al_load_bitmap("src/imagenes/fondomenu.jpg");
+    fondo = al_load_bitmap("C:\\Users\\josed\\CLionProjects\\DonCEy-Kong-Jr\\src\\tec\\ac\\cr\\client\\src\\imagenes\\fondomenu.png");
+    al_draw_bitmap(fondo, 0, 0, 0);
+    al_set_target_bitmap(al_get_backbuffer(pantalla));
+    assert(fondo != NULL);
 }
