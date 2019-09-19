3e0eeafb8b38eda151640b9b76f1fb6b76689037
Jose David <jose.david.san@gmail.com>
2019-09-19 14:49:18 -0600

    Se trabaja la interfaz. Creando listas de structs para dibujar elementos

---
+++ src/tec/ac/cr/client/src/bitmapNode.c
@@ -0,0 +1,90 @@
+//
+// Created by josed on 19/9/2019.
+//
+
+#include "bitmapNode.h"
+
+void insertVine(ALLEGRO_BITMAP* bitmap){
+    struct BitMapNode* tmp = (struct BitMapNode*) malloc(sizeof(struct BitMapNode));
+    tmp->bitmap = bitmap;
+    tmp->next = vineHead;
+    vineHead = tmp;
+}
+
+struct BitMapNode* searchVine(int position){
+    struct BitMapNode* tmp = vineHead;
+    int i = 0;
+    while (i != position){
+        tmp = tmp->next;
+        i++;
+    }
+    return tmp;
+}
+
+void insertBlueKremlin(ALLEGRO_BITMAP* bitmap){
+    struct BitMapNode* tmp = (struct BitMapNode*) malloc(sizeof(struct BitMapNode));
+    tmp->bitmap = bitmap;
+    tmp->next = blueKremlinHead;
+    vineHead = tmp;
+}
+
+struct BitMapNode* searchBlueKremlin (int position){
+    struct BitMapNode* tmp = blueKremlinHead;
+    int i = 0;
+    while (i != position){
+        tmp = tmp->next;
+        i++;
+    }
+    return tmp;
+}
+
+void insertRedKremlin(ALLEGRO_BITMAP* bitmap){
+    struct BitMapNode* tmp = (struct BitMapNode*) malloc(sizeof(struct BitMapNode));
+    tmp->bitmap = bitmap;
+    tmp->next = redKremlinHead;
+    vineHead = tmp;
+}
+
+struct BitMapNode* searchRedKremlin (int position){
+    struct BitMapNode* tmp = redKremlinHead;
+    int i = 0;
+    while (i != position){
+        tmp = tmp->next;
+        i++;
+    }
+    return tmp;
+}
+
+void insertFruit (ALLEGRO_BITMAP* bitmap){
+    struct BitMapNode* tmp = (struct BitMapNode*) malloc(sizeof(struct BitMapNode));
+    tmp->bitmap = bitmap;
+    tmp->next = fruitHead;
+    vineHead = tmp;
+}
+
+struct BitMapNode* searchFruit (int position){
+    struct BitMapNode* tmp = fruitHead;
+    int i = 0;
+    while (i != position){
+        tmp = tmp->next;
+        i++;
+    }
+    return tmp;
+}
+
+void insertFloor (ALLEGRO_BITMAP* bitmap){
+    struct BitMapNode* tmp = (struct BitMapNode*) malloc(sizeof(struct BitMapNode));
+    tmp->bitmap = bitmap;
+    tmp->next = floorHead;
+    vineHead = tmp;
+}
+
+struct BitMapNode* searchFloor (int position){
+    struct BitMapNode* tmp = floorHead;
+    int i = 0;
+    while (i != position){
+        tmp = tmp->next;
+        i++;
+    }
+    return tmp;
+}
--- src/tec/ac/cr/client/src/gui.c
+++ src/tec/ac/cr/client/src/gui.c
@@ -5,9 +5,60 @@
 #include "gui.h"
 
 void init_screen(){
-    pantalla = al_create_display(ANCHO_PANTALLA, ALTO_PANTALLA);
+
+    pantalla = al_create_display(LARGO_PANTALLA, ANCHO_PANTALLA);
+
+    int largoXsprite = largo / 16;
+    int anchoYsprite = ancho / 24;
+
+    printf("%d" , largoXsprite);
+    printf("%d" , anchoYsprite);
+
     fondo = al_load_bitmap("C:\\Users\\josed\\CLionProjects\\DonCEy-Kong-Jr\\src\\tec\\ac\\cr\\client\\src\\imagenes\\fondomenu.png");
+
     al_draw_bitmap(fondo, 0, 0, 0);
     al_set_target_bitmap(al_get_backbuffer(pantalla));
     assert(fondo != NULL);
+
+}
+void dibujarMatriz(){
+    for(int i=0; i<=24; i++){
+        for(int j=0; i<=16; j++){
+            if (gameMatrix[i][j] == 0){
+                printf("buscar en lista de lie");
+            }
+            if (gameMatrix[i][j] == 211){
+                printf("pintar enemigo 1");
+            }
+            if (gameMatrix[i][j] == 212){
+                printf("pintar enemigo 2");
+            }
+            if (gameMatrix[i][j] == 3){
+                printf("pintar negro fondo");
+            }
+            if (gameMatrix[i][j] == 4){
+                printf("pintar negro fondo");
+            }
+            if (gameMatrix[i][j] == 5){
+                printf("pintar negro fondo");
+            }
+            if (gameMatrix[i][j] == 6){
+                printf("pintar negro fondo");
+            }
+            if (gameMatrix[i][j] == 7){
+                printf("pintar negro fondo");
+            }
+            if (gameMatrix[i][j] == 8){
+                printf("pintar negro fondo");
+            }
+            if (gameMatrix[i][j] == 9){
+                printf("pintar negro fondo");
+            }
+            if (gameMatrix[i][j] == 10){
+                printf("pintar negro fondo");
+            }
+        }
+    }
+
+
 }
