5dadca44dca8fd812e8bdc057e0e90961e07db63
Jose David <jose.david.san@gmail.com>
2019-09-20 02:35:20 -0600

    Se trabaja la interfaz. Creando funciones para dibujar.

---
+++ src/tec/ac/cr/client/cmake-build-debug/CMakeFiles/DonceyKongJr.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DonceyKongJr.dir/src/main.c.obj"
+  "CMakeFiles/DonceyKongJr.dir/src/gui.c.obj"
+  "CMakeFiles/DonceyKongJr.dir/src/bitmapNode.c.obj"
+  "DonceyKongJr.pdb"
+  "DonceyKongJr.exe"
+  "DonceyKongJr.exe.manifest"
+  "libDonceyKongJr.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DonceyKongJr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/tec/ac/cr/client/cmake-build-default/CMakeFiles/DonceyKongJr.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DonceyKongJr.dir/src/main.c.obj"
+  "DonceyKongJr.pdb"
+  "DonceyKongJr.exe"
+  "DonceyKongJr.exe.manifest"
+  "DonceyKongJr.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DonceyKongJr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
