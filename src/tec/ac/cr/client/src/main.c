8bc190d54baf0e59594d775b78b135e1eb49ad11
Kevin <skryfall@gmail.com>
2019-09-20 09:27:50 -0600

    Bugfixes

--- src/tec/ac/cr/client/src/main.c
+++ src/tec/ac/cr/client/src/main.c
@@ -8,39 +8,96 @@
 #include "gui.h"
 #include "logic/Game.h"
 
-int main() {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+
+enum MYKEYS {
+    KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_SPACE
+};
+
+static int largoPantalla = 700;
+static int anchoPantalla = 700;
+
+bool key[5];
+ALLEGRO_DISPLAY* display = NULL;
+ALLEGRO_EVENT_QUEUE* event_queue = NULL;
+ALLEGRO_TIMER* timer = NULL;
+bool redraw;
+bool done;
+
+int init_game() {
+
+    key[1] = false;
+    key[2] = false;
+    key[3] = false;
+    key[4] = false;
+    key[5] = false;
+    redraw = true;
 
     al_init();
 
-    init_matrix();
-
+    display = al_create_display(largoPantalla, anchoPantalla);
     al_init_primitives_addon();
     al_init_image_addon();
-    init_screen();
-
-    ALLEGRO_EVENT_QUEUE *queue;
-    queue = al_create_event_queue();
-
     al_install_keyboard();
-    al_register_event_source(queue, al_get_keyboard_event_source());
-    al_register_event_source(queue, al_get_display_event_source(pantalla));
 
-    createVines();
-    createBlueKremlins();
-    createRedKremlins();
-    createFruits();
-    createFloors();
+    al_flip_display();
 
-    bool corriendo = true;
-    while (corriendo){
-
-        al_flip_display();
-
-        //ALLEGRO_EVENT event;
-        //al_wait_for_event(queue, &event);
-        //if (event.type == ALLEGRO_EVENT_KEY_UP || event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
-          //  corriendo = false;
-        dibujarMatriz();
-    }
-    al_destroy_display(pantalla);
+    timer = al_create_timer(1.0 / 60);
+    event_queue = al_create_event_queue();
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
+    al_register_event_source(event_queue, al_get_timer_event_source(timer));
+    al_start_timer(timer);
+    return 0;
 }
+
+
+
+int run(){
+    int action = -1;
+    ALLEGRO_KEYBOARD_STATE keyboardState;
+    while(!done) {
+        ALLEGRO_EVENT ev;
+        al_wait_for_event(event_queue, &ev);
+        if (ev.type == ALLEGRO_EVENT_KEY_UP) {
+            switch(ev.keyboard.keycode) {
+                case ALLEGRO_KEY_ESCAPE:
+                    done = true;
+            }
+        }
+        if(ev.type == ALLEGRO_EVENT_TIMER ) {
+            al_get_keyboard_state(&keyboardState);
+            if (al_key_down(&keyboardState, ALLEGRO_KEY_UP)){
+                action = -1;
+            }else if (al_key_down(&keyboardState,ALLEGRO_KEY_DOWN)){
+                action = -1;
+            }else if (al_key_down(&keyboardState,ALLEGRO_KEY_RIGHT)){
+                action = 1;
+            }else if (al_key_down(&keyboardState,ALLEGRO_KEY_LEFT)){
+                action = 2;
+            }
+            redraw = true;
+        }
+        if (redraw && al_is_event_queue_empty(event_queue)){
+            redraw = false;
+            dibujarMatriz(action, display);
+            al_flip_display();
+        }
+    }
+    al_destroy_display(display);
+    al_destroy_timer(timer);
+    al_destroy_event_queue(event_queue);
+    return 0;
+}
+
+int main() {
+
+  //  init_screen();
+    init_game();
+    init_matrix();
+    run();
+
+    return 0;
+}
+
+#pragma clang diagnostic pop
