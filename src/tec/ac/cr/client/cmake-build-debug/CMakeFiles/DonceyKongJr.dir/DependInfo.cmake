f6346edb17629a8f6ead65c433d333239ba91279
Jose David <jose.david.san@gmail.com>
2019-09-20 02:35:20 -0600

    Se trabaja la interfaz. Creando funciones para dibujar.

---
+++ src/tec/ac/cr/client/cmake-build-debug/CMakeFiles/DonceyKongJr.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/josed/CLionProjects/DonCEy-Kong-Jr/src/tec/ac/cr/client/src/bitmapNode.c" "C:/Users/josed/CLionProjects/DonCEy-Kong-Jr/src/tec/ac/cr/client/cmake-build-debug/CMakeFiles/DonceyKongJr.dir/src/bitmapNode.c.obj"
+  "C:/Users/josed/CLionProjects/DonCEy-Kong-Jr/src/tec/ac/cr/client/src/gui.c" "C:/Users/josed/CLionProjects/DonCEy-Kong-Jr/src/tec/ac/cr/client/cmake-build-debug/CMakeFiles/DonceyKongJr.dir/src/gui.c.obj"
+  "C:/Users/josed/CLionProjects/DonCEy-Kong-Jr/src/tec/ac/cr/client/src/main.c" "C:/Users/josed/CLionProjects/DonCEy-Kong-Jr/src/tec/ac/cr/client/cmake-build-debug/CMakeFiles/DonceyKongJr.dir/src/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
