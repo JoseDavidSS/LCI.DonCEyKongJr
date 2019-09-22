d2684057c77564f68834561429e4262b840162c7
Kevin <skryfall@gmail.com>
2019-09-21 21:01:46 -0600

    Agregado a DK, Mario y la llave en C, en Java se actualizo la interfaz

--- src/tec/ac/cr/server/src/logic/Fruit.java
+++ src/tec/ac/cr/server/src/logic/Fruit.java
@@ -2,12 +2,20 @@ package logic;
 
 public class Fruit extends Entity {
 
-    public Fruit(Integer posI, Integer posJ, Integer pointsValue){
+    public Fruit(Integer posI, Integer posJ, Integer pointsValue, Integer type){
         super(posI, posJ);
         this.pointsValue = pointsValue;
+        this.id = 2;
+        this.type = type;
+        this.placed = 0;
+        this.onScreen = 1;
     }
 
     private Integer pointsValue;
+    private Integer id;
+    private Integer type;
+    private Integer placed;
+    private Integer onScreen;
 
     public Integer getPointsValue() {
         return pointsValue;
@@ -15,5 +23,37 @@ public class Fruit extends Entity {
 
     public void setPointsValue(Integer pointsValue) {
         this.pointsValue = pointsValue;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getPlaced() {
+        return placed;
+    }
+
+    public void setPlaced(Integer placed) {
+        this.placed = placed;
+    }
+
+    public Integer getOnScreen() {
+        return onScreen;
+    }
+
+    public void setOnScreen(Integer onScreen) {
+        this.onScreen = onScreen;
     }
 }
