acace5d7506b75508ff61d03247dca162c9b1b91
Kevin <skryfall@gmail.com>
2019-09-22 23:53:23 -0600

    Documentacion Interna

--- src/tec/ac/cr/server/src/socket/Serializer.java
+++ src/tec/ac/cr/server/src/socket/Serializer.java
@@ -8,12 +8,24 @@ import java.io.IOException;
 
 public class Serializer {
 
+    /**
+     * Serializa un kremlin
+     * @param kremlin
+     * @return
+     * @throws IOException
+     */
     public static String serializeKremlin(Kremlin kremlin) throws IOException {
         ObjectMapper kremlinMapper = new ObjectMapper();
         return kremlinMapper.writeValueAsString(kremlin);
 
     }
 
+    /**
+     * Serializa un kremlin
+     * @param fruit
+     * @return
+     * @throws IOException
+     */
     public static String serializeFruit(Fruit fruit) throws IOException{
         ObjectMapper fruit_Mapper = new ObjectMapper();
         return fruit_Mapper.writeValueAsString(fruit);
